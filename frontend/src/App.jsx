23813c365f551598980db55aad61360abcfaa2d1
Dmartins2695 <62670499+Dmartins2695@users.noreply.github.com>
2024-05-29 22:48:38 +0100

    feat: frontend start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,13 @@
-import { useState } from 'react'
-import useAuth from './hooks/useAuth'
 import axios from 'axios'
+import { useState } from 'react'
+import { Outlet, Route, Routes } from 'react-router-dom'
+import useAuth from './hooks/useAuth'
+import Home from './pages/home'
+import ErrorPage from './pages/errorPage'
+import Dashboard from './pages/dashboard'
+import Budget from './pages/budget'
+import SideMenu from './components/sideMenu'
+import { Box, Grid } from '@mui/material'
 
 axios.defaults.baseURL = 'http://localhost:5000'
 
@@ -33,10 +40,39 @@ const Public = () => {
 	return <>OUT OF THE APP</>
 }
 
-function App() {
+function Dummy() {
 	const { isLogin, token } = useAuth()
 
 	return <>{isLogin ? <Private token={token} /> : <Public />}</>
+}
+
+const PermanentElementsWrapper = () => {
+	return (
+		<Grid container>
+			<Grid item xs={4}>
+				<SideMenu />
+			</Grid>
+			<Grid item xs>
+				<Outlet />
+			</Grid>
+		</Grid>
+	)
+}
+
+const App = () => {
+	return (
+		<div className='App'>
+			<Routes>
+				<Route element={<PermanentElementsWrapper />}>
+					<Route path='/dashboard' element={<Dashboard />} />
+					<Route path='/budgets' element={<Budget />} />
+					<Route path='/' element={<Home />} />
+					<Route path='/*' element={<Home />} />
+					<Route path='/404' element={<ErrorPage />} />
+				</Route>
+			</Routes>
+		</div>
+	)
 }
 
 export default App
---
+++ frontend/src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideMenu from '../../components/sideMenu'
+
+
+
+const Home = () => {
+	return (
+		<></>
+	)
+}
+
+export default Home
